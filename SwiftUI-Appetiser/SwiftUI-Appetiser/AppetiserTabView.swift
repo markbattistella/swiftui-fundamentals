e3708f63c39f2be4a786fc0df8bc0152f6242118
Mark Battistella <markbattistella@users.noreply.github.com>
2021-10-14 08:23:38 +1100

    beginning

---
+++ SwiftUI-Appetiser/SwiftUI-Appetiser/AppetiserTabView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Appetiser
+//
+//  Created by Mark Battistella on 13/10/21.
+//
+
+import SwiftUI
+
+struct AppetiserTabView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetiserTabView()
+    }
+}
+
+
---
+++ SwiftUI-Appetiser/SwiftUI-Appetiser/SwiftUI_AppetisersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetisersApp.swift
+//  SwiftUI-Appetisers
+//
+//  Created by Mark Battistella on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetisersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetiserTabView()
+        }
+    }
+}
